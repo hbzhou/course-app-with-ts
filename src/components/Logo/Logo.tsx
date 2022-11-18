cfcab69bd24853b160eba2d7fa78737c4c4233ae
Jeremy_Zhou <Jeremy_Zhou@epam.com>
2022-11-18 12:22:08 +0800

    feat: add tailwindcss config

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import "./logo.css";
 
 const Logo: React.FC = () => {
   const logo = require("./logo.png");
   return (
-    <div className="logo">
-      <img className="img" src={logo} alt="logo" />
+    <div>
+      <img src={logo} alt="logo" />
     </div>
   );
 };
