7df647a78f90217fefe29a460d1e7027ca94bf12
Jeremy Zhou <jeremygilbert@126.com>
2022-12-05 21:57:44 +0800

    feat: add popup modal

--- src/components/Authors/AddAuthor.tsx
+++ src/components/Authors/AddAuthor.tsx
@@ -1,17 +1,11 @@
-import React from "react";
-import Button from "../../common/Button/Button";
 import Input from "../../common/Input/Input";
 
 const AddAuthor = () => {
   return (
-    <div className="m-4  ">
-      <h1 className="text-center text-2xl font-bold my-2">Add author</h1>
+    <div className="m-4">
       <div>
         <div>Author name</div>
         <Input className=" border-amber-300 p-2 my-2 rounded-md w-96" placeholder="Enter author name" />
-        <div>
-          <Button className=" border-purple-300 rounded-md w-36 my-2">Create author</Button>
-        </div>
       </div>
     </div>
   );
--- src/components/Authors/Authors.tsx
+++ src/components/Authors/Authors.tsx
@@ -6,28 +6,30 @@ import AddAuthor from "./AddAuthor";
 import AuthorItem from "./AuthorItem";
 
 const Authors = () => {
-  const [modal, setModal] = useState<boolean>(false);
-  const open = () => {
-    console.log("open dialog");
-  };
+  const [showModal, setShowModal] = useState<boolean>(false);
   return (
     <div className=" border-solid border-2 border-pink-400 m-4 flex justify-center">
       <div className="m-4 w-1/2">
         <div>
           <div className="flex justify-center my-8">
             <div className="block font-bold text-2xl ml-32">Authors</div>
-            <Button className="ml-24 rounded-md border-purple-400" onClick={open}>
+            <Button className="ml-24 rounded-md border-purple-400" onClick={() => setShowModal(true)}>
               Create author
             </Button>
           </div>
           {authors.map((author) => (
-            <AuthorItem {...author} />
+            <AuthorItem key={author.id} {...author} />
           ))}
         </div>
       </div>
-      <Modal show={modal} handleClose={() => setModal(false)}>
-        <AddAuthor />
-      </Modal>
+      {showModal ? (
+        <Modal
+          title="Add Author"
+          children={<AddAuthor />}
+          handleClose={() => setShowModal(false)}
+          handleSave={() => setShowModal(false)}
+        />
+      ) : null}
     </div>
   );
 };
