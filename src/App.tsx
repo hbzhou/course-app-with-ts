ae31a192003ac8a6d8085ced78108f4339319533
Jeremy_Zhou <Jeremy_Zhou@epam.com>
2022-12-06 20:54:31 +0800

    feat:add routes for courses & authors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Authors from "./components/Authors/Authors";
 import Courses from "./components/Courses/Courses";
 import CreateCourse from "./components/CreateCourse/CreateCourse";
 import Header from "./components/Header/Header";
@@ -10,8 +11,10 @@ const App: React.FC = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Courses />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/authors" element={<Authors />} />
+        <Route path="/createCourse" element={<CreateCourse />} />
       </Routes>
-      <CreateCourse />
     </BrowserRouter>
   );
 };
