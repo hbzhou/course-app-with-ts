6777f34e1ee907fb347a00079f64d39484ecfab7
Jeremy_Zhou <Jeremy_Zhou@epam.com>
2022-12-06 18:03:58 +0800

    feat:add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Courses from "./components/Courses/Courses";
 import CreateCourse from "./components/CreateCourse/CreateCourse";
 import Header from "./components/Header/Header";
 
 const App: React.FC = () => {
   return (
-    <React.Fragment>
+    <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path="/" element={<Courses />} />
+      </Routes>
       <CreateCourse />
-    </React.Fragment>
+    </BrowserRouter>
   );
 };
 
