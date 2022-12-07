9d6d8dd1b9ee94d6aecbbd474dcac0062a5a5505
Jeremy_Zhou <Jeremy_Zhou@epam.com>
2022-12-07 21:10:06 +0800

    feat: add style for course info component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Authors from "./components/Authors/Authors";
+import CourseInfo from "./components/CourseInfo/CourseInfo";
 import Courses from "./components/Courses/Courses";
 import CreateCourse from "./components/CreateCourse/CreateCourse";
 import Header from "./components/Header/Header";
 import Login from "./components/Login/Login";
 import Registration from "./components/Registration/Registration";
+const course = {
+  title: "title",
+  description: "description",
+  creationDate: "9/3/2021",
+  duration: 30,
+  authors: ["9b87e8b8-6ba5-40fc-a439-c4e30a373d36"],
+  id: "66cc289e-6de9-49b2-9ca7-8b4f409d6467",
+};
 
 const App: React.FC = () => {
   return (
@@ -14,6 +23,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Courses />} />
         <Route path="/courses" element={<Courses />} />
+        <Route path="/courses/:id" element={<CourseInfo {...course} />} />
         <Route path="/authors" element={<Authors />} />
         <Route path="/createCourse" element={<CreateCourse />} />
         <Route path="/login" element={<Login />} />
--- src/components/CourseInfo/CourseInfo.tsx
+++ src/components/CourseInfo/CourseInfo.tsx
@@ -4,28 +4,30 @@ import { Course } from "../Courses/CourseCard";
 
 const CourseInfo: React.FC<Course> = (course: Course) => {
   return (
-    <div>
+    <div className=" border-solid border-2 border-blue-300 m-4">
       <Title>{course.title}</Title>
-      <div>
-        <div>{course.description}</div>
-        <div>
+      <div className="flex">
+        <div className=" w-1/2 m-4">{course.description}</div>
+        <div className="m-4">
           <div>
-            <span>ID:</span>
+            <span className="font-bold mr-3">ID:</span>
             <span>{course.id}</span>
           </div>
           <div>
-            <span>Duration:</span>
+            <span className="font-bold mr-3">Duration:</span>
             <span>{course.duration} hours</span>
           </div>
           <div>
-            <span>Created:</span>
+            <span className="font-bold mr-3">Created:</span>
             <span>{course.creationDate}</span>
           </div>
-          <div>
-            <div>Authors:</div>
-            {course.authors.map((author) => {
-              return <div key={author}>author</div>;
-            })}
+          <div className="flex">
+            <div className="font-bold mr-3">Authors:</div>
+            <div>
+              {course.authors.map((author) => {
+                return <div key={author}>author</div>;
+              })}
+            </div>
           </div>
         </div>
       </div>
