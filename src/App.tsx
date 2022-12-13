91ec391a401e13878d1f64c99e528ed577f21601
Jeremy_Zhou <Jeremy_Zhou@epam.com>
2022-12-13 21:17:46 +0800

    feat: add redux

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,6 @@ import CreateCourse from "./components/CreateCourse/CreateCourse";
 import Header from "./components/Header/Header";
 import Login from "./components/Login/Login";
 import Registration from "./components/Registration/Registration";
-const course = {
-  title: "title",
-  description: "description",
-  creationDate: "9/3/2021",
-  duration: 30,
-  authors: ["9b87e8b8-6ba5-40fc-a439-c4e30a373d36"],
-  id: "66cc289e-6de9-49b2-9ca7-8b4f409d6467",
-};
 
 const App: React.FC = () => {
   return (
@@ -23,7 +15,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Courses />} />
         <Route path="/courses" element={<Courses />} />
-        <Route path="/courses/:id" element={<CourseInfo {...course} />} />
+        <Route path="/courses/:id" element={<CourseInfo />} />
         <Route path="/authors" element={<Authors />} />
         <Route path="/createCourse" element={<CreateCourse />} />
         <Route path="/login" element={<Login />} />
--- src/components/CourseInfo/CourseInfo.tsx
+++ src/components/CourseInfo/CourseInfo.tsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import Title from "../../common/Title/Title";
 import { Course } from "../Courses/CourseCard";
 
-const CourseInfo: React.FC<Course> = (course: Course) => {
+interface QueryCourseResponse {
+  successful: boolean;
+  result: Course;
+}
+
+const CourseInfo: React.FC = () => {
+  const { id } = useParams();
+  const [course, setCourse] = useState<Course>();
+  const getCourse = async () => {
+    const token = localStorage.getItem("token") ?? "";
+    const response = await fetch(`/courses/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    });
+    const queryCourseResp: QueryCourseResponse = await response.json();
+    if (queryCourseResp.successful) {
+      setCourse(queryCourseResp.result);
+    }
+  };
+
+  useEffect(() => {
+    getCourse();
+  });
+  if (!course) {
+    return <div>Loading</div>;
+  }
   return (
     <div className=" border-solid border-2 border-blue-300 m-4">
       <Title>{course.title}</Title>
       <div className="flex">
-        <div className=" w-1/2 m-4">{course.description}</div>
+        <div className=" w-1/2 m-4">{course!.description}</div>
         <div className="m-4">
           <div>
             <span className="font-bold mr-3">ID:</span>
