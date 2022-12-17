0b7254d1e5e29949c6319ab9b6a0db7dc61d63ff
Jeremy Zhou <jeremygilbert@126.com>
2022-12-17 17:38:03 +0800

    feat:update the style of code

--- src/components/Courses/CourseCard.tsx
+++ src/components/Courses/CourseCard.tsx
@@ -7,26 +7,26 @@ const CourseCard: React.FC<Course> = ({ id, title, description, duration, creati
   const navigate = useNavigate();
 
   return (
-    <div className="flex justify-between items-center m-4 border-solid border-2 border-green-500">
-      <div className="flex flex-col m-4 w-1/2 ">
+    <div className='flex justify-between items-center m-4 border-solid border-2 border-green-500'>
+      <div className='flex flex-col m-4 w-1/2 '>
         <h1>{title}</h1>
         <div>{description}</div>
       </div>
-      <div className="flex flex-col justify-evenly flex-grow m-4 min-w-fit">
-        <div className="m-2">
+      <div className='flex flex-col justify-evenly flex-grow m-4 min-w-fit'>
+        <div className='m-2'>
           <label>Authors:</label>
           <span>{authors.join(",")}</span>
         </div>
-        <div className="m-2">
+        <div className='m-2'>
           <label>Duration:</label>
           <span>{duration} hours</span>
         </div>
-        <div className="m-2">
+        <div className='m-2'>
           <label>Created:</label>
           <span>{creationDate}</span>
         </div>
-        <div className="m-2 text-center">
-          <Button className=" border-purple-500 min-w-max w-52" onClick={() => navigate(`/courses/${id}`)}>
+        <div className='m-2 text-center'>
+          <Button className=' border-purple-500 min-w-max w-52' onClick={() => navigate(`/courses/${id}`)}>
             Show course
           </Button>
         </div>
